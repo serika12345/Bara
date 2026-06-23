8761ee10a8b0490a99377d9160f012467b2378c8
serika12345 <281916063+serika12345@users.noreply.github.com>
2026-06-23 14:52:09 +0900

    refactor: split macho image module

--- crates/bara-runtime/src/guest_image/mod.rs
+++ crates/bara-runtime/src/guest_image/mod.rs
@@ -1,11 +1,14 @@
 use bara_ir::{
-    ProgramImageImports, ProgramImageMappedBytes, ProgramImageMetadata, ProgramImageRange,
-    ProgramImageRelocations, ProgramImageSectionKind, ProgramImageSections, ProgramImageSymbols,
-    ProgramUnwindMetadata, X86Va,
+    ProgramImageImports, ProgramImageMappedBytes, ProgramImageRange, ProgramImageRelocations,
+    ProgramImageSections, ProgramImageSymbols, ProgramUnwindMetadata, X86Va,
 };
 
+mod mach_o;
 mod metadata;
 
+pub use mach_o::{
+    MachOExecutableCodeRange, MachOExecutableCodeSegment, MachOExecutableEntryPoint, MachOImage,
+};
 pub use metadata::{
     GuestImageImports, GuestImageMappedBytes, GuestImageMappedBytesSource, GuestImageMetadata,
     GuestImageRelocations, GuestImageSections, GuestImageSymbols, GuestImageUnwindMetadata,
@@ -109,150 +112,6 @@ pub enum GuestImageFormat {
     MachO,
 }
 
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct MachOImage {
-    guest_image: GuestImage,
-    code_segment: MachOExecutableCodeSegment,
-}
-
-impl MachOImage {
-    pub fn executable(
-        entry_point: MachOExecutableEntryPoint,
-        code_segment: MachOExecutableCodeSegment,
-        metadata: GuestImageMetadata,
-    ) -> Result<Self, GuestImageError> {
-        Ok(Self {
-            guest_image: GuestImage::mach_o_executable(
-                entry_point.guest_image_entry_point(),
-                code_segment.guest_image_segment(),
-                metadata,
-            )?,
-            code_segment,
-        })
-    }
-
-    pub fn executable_from_code_range(
-        entry_point: MachOExecutableEntryPoint,
-        code_range: MachOExecutableCodeRange,
-        metadata: GuestImageMetadata,
-    ) -> Result<Self, GuestImageError> {
-        Self::executable(
-            entry_point,
-            MachOExecutableCodeSegment::new(code_range),
-            metadata,
-        )
-    }
-
-    pub fn executable_from_program_image_metadata(
-        entry_point: MachOExecutableEntryPoint,
-        metadata: &ProgramImageMetadata,
-    ) -> Result<Self, GuestImageError> {
-        let code_range = MachOExecutableCodeRange::from_program_image_metadata(metadata)?;
-        Self::executable_from_code_range(
-            entry_point,
-            code_range,
-            GuestImageMetadata::from_program_image_metadata(metadata),
-        )
-    }
-
-    pub const fn guest_image(&self) -> &GuestImage {
-        &self.guest_image
-    }
-
-    pub const fn entry_point(&self) -> MachOExecutableEntryPoint {
-        MachOExecutableEntryPoint::new(self.guest_image.entry_point().address())
-    }
-
-    pub const fn code_segment(&self) -> MachOExecutableCodeSegment {
-        self.code_segment
-    }
-
-    pub const fn metadata(&self) -> &GuestImageMetadata {
-        self.guest_image.metadata()
-    }
-}
-
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub struct MachOExecutableEntryPoint {
-    address: X86Va,
-}
-
-impl MachOExecutableEntryPoint {
-    pub const fn new(address: X86Va) -> Self {
-        Self { address }
-    }
-
-    pub const fn address(self) -> X86Va {
-        self.address
-    }
-
-    const fn guest_image_entry_point(self) -> GuestImageEntryPoint {
-        GuestImageEntryPoint::new(self.address)
-    }
-}
-
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub struct MachOExecutableCodeRange {
-    range: ProgramImageRange,
-}
-
-impl MachOExecutableCodeRange {
-    pub const fn new(range: ProgramImageRange) -> Self {
-        Self { range }
-    }
-
-    pub fn from_program_image_metadata(
-        metadata: &ProgramImageMetadata,
-    ) -> Result<Self, GuestImageError> {
-        let mut code_sections = metadata
-            .sections()
-            .items()
-            .iter()
-            .filter(|section| section.kind() == ProgramImageSectionKind::Code);
-        let code_section = code_sections
-            .next()
-            .ok_or(GuestImageError::MissingMachOExecutableCodeSection)?;
-        if code_sections.next().is_some() {
-            return Err(GuestImageError::AmbiguousMachOExecutableCodeSections);
-        }
-
-        Ok(Self::new(code_section.range()))
-    }
-
-    pub const fn range(self) -> ProgramImageRange {
-        self.range
-    }
-}
-
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub struct MachOExecutableCodeSegment {
-    segment: GuestImageSegment,
-}
-
-impl MachOExecutableCodeSegment {
-    pub const fn new(range: MachOExecutableCodeRange) -> Self {
-        Self {
-            segment: GuestImageSegment::mach_o_executable_code(range),
-        }
-    }
-
-    pub const fn range(self) -> MachOExecutableCodeRange {
-        MachOExecutableCodeRange::new(self.segment.range())
-    }
-
-    pub const fn source(self) -> GuestImageSegmentSource {
-        self.segment.source()
-    }
-
-    pub const fn address_space(self) -> GuestImageAddressSpace {
-        self.segment.address_space()
-    }
-
-    const fn guest_image_segment(self) -> GuestImageSegment {
-        self.segment
-    }
-}
-
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub struct GuestImageEntryPoint {
     address: X86Va,
@@ -311,15 +170,6 @@ pub struct GuestImageSegment {
 }
 
 impl GuestImageSegment {
-    pub const fn mach_o_executable_code(range: MachOExecutableCodeRange) -> Self {
-        Self::new(
-            GuestImageSegmentKind::Code,
-            range.range(),
-            GuestImageSegmentSource::LcSegment64FileRange,
-            GuestImageAddressSpace::MachOVirtualAddress,
-        )
-    }
-
     pub const fn new(
         kind: GuestImageSegmentKind,
         range: ProgramImageRange,
